6eccece14f1f6fc6e451b1e0409cc85fd0dc7f39
Emil Boman <emil.boman@gmail.com>
2017-05-15 23:10:09 +0200

    Iteration of entities in map instantiation

--- src/stirling/map.cpp
+++ src/stirling/map.cpp
@@ -40,79 +40,79 @@ namespace stirling {
 
         uint64_t next_dynamic_index = 0;
 
-        // Entity create info
-        auto create_info = create_info_list.at(0);
+        // Iterate through all entities to be created.
+        for (auto create_info : create_info_list) {
+            // Descriptor set layout
+            auto descriptor_set_layout = initDescriptorSetLayout(device);
 
-        // Descriptor set layout
-        auto descriptor_set_layout = initDescriptorSetLayout(device);
+            // Descriptor pool
+            map_instance.descriptor_pools.emplace_back(initDescriptorPool(device));
+            auto& descriptor_pool = map_instance.descriptor_pools.back();
 
-        // Descriptor pool
-        map_instance.descriptor_pools.emplace_back(initDescriptorPool(device));
-        auto& descriptor_pool = map_instance.descriptor_pools.back();
+            // Shaders
+            vulkan::ShaderModule vertex_shader(device, create_info.vertex_shader_file);
+            vulkan::ShaderModule fragment_shader(device, create_info.fragment_shader_file);
+            std::vector<VkPipelineShaderStageCreateInfo> shader_stages = {
+                vulkan::initializers::pipelineShaderStageCreateInfo(VK_SHADER_STAGE_VERTEX_BIT, vertex_shader, "main"),
+                vulkan::initializers::pipelineShaderStageCreateInfo(VK_SHADER_STAGE_FRAGMENT_BIT, fragment_shader, "main"),
+            };
 
-        // Shaders
-        vulkan::ShaderModule vertex_shader(device, create_info.vertex_shader_file);
-        vulkan::ShaderModule fragment_shader(device, create_info.fragment_shader_file);
-        std::vector<VkPipelineShaderStageCreateInfo> shader_stages = {
-            vulkan::initializers::pipelineShaderStageCreateInfo(VK_SHADER_STAGE_VERTEX_BIT, vertex_shader, "main"),
-            vulkan::initializers::pipelineShaderStageCreateInfo(VK_SHADER_STAGE_FRAGMENT_BIT, fragment_shader, "main"),
-        };
+            // Pipeline
+            map_instance.pipelines.emplace_back(vulkan::Pipeline(device, { descriptor_set_layout }, render_pass, extent, shader_stages));
+            auto& pipeline = map_instance.pipelines.back();
 
-        // Pipeline
-        map_instance.pipelines.emplace_back(vulkan::Pipeline(device, { descriptor_set_layout }, render_pass, extent, shader_stages));
-        auto& pipeline = map_instance.pipelines.back();
+            // Model
+            auto model = vulkan::Model::loadFromFile(device, create_info.model_file, create_info.texture_file);
+            auto& vertex_buffer = model.vertex_buffer;
+            auto& index_buffer = model.index_buffer;
+            auto& texture = model.texture;
 
-        // Model
-        auto model = vulkan::Model::loadFromFile(device, create_info.model_file, create_info.texture_file);
-        auto& vertex_buffer = model.vertex_buffer;
-        auto& index_buffer = model.index_buffer;
-        auto& texture = model.texture;
-
-        // Descriptor set
-        auto descriptor_set = descriptor_pool.allocateDescriptorSet(descriptor_set_layout);
+            // Descriptor set
+            auto descriptor_set = descriptor_pool.allocateDescriptorSet(descriptor_set_layout);
         
-        VkDescriptorImageInfo image_info = {};
-        image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-        image_info.imageView   = texture.image_view;
-        image_info.sampler     = texture.sampler;
+            VkDescriptorImageInfo image_info = {};
+            image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+            image_info.imageView   = texture.image_view;
+            image_info.sampler     = texture.sampler;
 
-        std::array<VkWriteDescriptorSet, 3> write_descriptor_sets = {
-            vulkan::initializers::writeDescriptorSet(descriptor_set, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, &map_instance.static_uniform_buffer.m_descriptor),
-            vulkan::initializers::writeDescriptorSet(descriptor_set, 1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, &map_instance.dynamic_uniform_buffer.m_descriptor),
-            vulkan::initializers::writeDescriptorSet(descriptor_set, 2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, &image_info)
-        };
-        vkUpdateDescriptorSets(device, write_descriptor_sets.size(), write_descriptor_sets.data(), 0, nullptr);
+            std::array<VkWriteDescriptorSet, 3> write_descriptor_sets = {
+                vulkan::initializers::writeDescriptorSet(descriptor_set, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, &map_instance.static_uniform_buffer.m_descriptor),
+                vulkan::initializers::writeDescriptorSet(descriptor_set, 1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, &map_instance.dynamic_uniform_buffer.m_descriptor),
+                vulkan::initializers::writeDescriptorSet(descriptor_set, 2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, &image_info)
+            };
+            vkUpdateDescriptorSets(device, write_descriptor_sets.size(), write_descriptor_sets.data(), 0, nullptr);
 
-        // Transform (Dynamic uniform buffer)
-        uint32_t dynamic_offset = next_dynamic_index++ * static_cast<uint32_t>(dynamic_alignment);
-        auto transform = (glm::mat4*)(((uint64_t)map_instance.dynamic_uniform_buffer_object.model + dynamic_offset));
+            // Transform (Dynamic uniform buffer)
+            uint32_t dynamic_offset = next_dynamic_index++ * static_cast<uint32_t>(dynamic_alignment);
+            auto transform = (glm::mat4*)(((uint64_t)map_instance.dynamic_uniform_buffer_object.model + dynamic_offset));
 
-        // Render instruction
-        RenderInstruction render_instruction;
-        render_instruction.pipeline_layout       = pipeline.getLayout();
-        render_instruction.pipeline              = pipeline;
-        render_instruction.vertex_buffers        = { vertex_buffer };
-        render_instruction.vertex_buffer_offsets = { 0 };
-        render_instruction.index_buffer          = index_buffer;
-        render_instruction.index_count           = index_buffer.size();
-        render_instruction.descriptor_sets       = { descriptor_set };
-        render_instruction.dynamic_offsets       = { dynamic_offset };
-        map_instance.render_instructions.emplace_back(render_instruction);
+            // Render instruction
+            RenderInstruction render_instruction;
+            render_instruction.pipeline_layout       = pipeline.getLayout();
+            render_instruction.pipeline              = pipeline;
+            render_instruction.vertex_buffers        = { vertex_buffer };
+            render_instruction.vertex_buffer_offsets = { 0 };
+            render_instruction.index_buffer          = index_buffer;
+            render_instruction.index_count           = index_buffer.size();
+            render_instruction.descriptor_sets       = { descriptor_set };
+            render_instruction.dynamic_offsets       = { dynamic_offset };
+            map_instance.render_instructions.emplace_back(render_instruction);
 
-        // Components
-        auto physics_component = new PhysicsComponent();
-        auto model_component = new ModelComponent(std::move(model), descriptor_set);
+            // Components
+            auto physics_component = new PhysicsComponent();
+            auto model_component = new ModelComponent(std::move(model), descriptor_set);
 
-        // Entity
-        map_instance.entities.emplace_back(Entity(vulkan::Transform(transform)));
-        auto& entity = map_instance.entities.back();
+            // Entity
+            map_instance.entities.emplace_back(Entity(vulkan::Transform(transform)));
+            auto& entity = map_instance.entities.back();
 
-        entity.transform().moveTo(create_info.position);
-        entity.transform().setRotation(create_info.rotation);
-        entity.transform().setScale(create_info.scale);
+            entity.transform().moveTo(create_info.position);
+            entity.transform().setRotation(create_info.rotation);
+            entity.transform().setScale(create_info.scale);
 
-        entity.addComponent(physics_component);
-        entity.addComponent(model_component);
+            entity.addComponent(physics_component);
+            entity.addComponent(model_component);
+        }
 
         return map_instance;
     }
